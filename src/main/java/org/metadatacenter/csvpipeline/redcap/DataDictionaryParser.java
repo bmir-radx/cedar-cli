3ec375dc5c7c2aa0de9ac8bde3062fadf702d03c
Matthew Horridge <matthew.horridge@stanford.edu>
2022-06-15 13:23:56 -0700

    A bit of documentation

--- src/main/java/org/metadatacenter/csvpipeline/redcap/DataDictionaryParser.java
+++ src/main/java/org/metadatacenter/csvpipeline/redcap/DataDictionaryParser.java
@@ -7,22 +7,40 @@ import com.fasterxml.jackson.dataformat.csv.CsvParser;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
  * 2022-06-15
+ *
+ * Parses a REDCap data dictionary that is formatted as a CSV file.  More details of this format can be found in the
+ * PDF here: https://www.utsouthwestern.edu/edumedia/edufiles/about_us/admin_offices/academic_information_services/redcap-database-creating-dictionary.pdf
  */
 public class DataDictionaryParser {
 
     public DataDictionaryParser() {
     }
 
+    /**
+     * Parse the data dictionary from the specified input stream.  The data dictionary is assumed to have a header row.
+     * @param inputStream The input stream
+     * @param handler A handler for handling parsed rows
+     */
     public void parse(InputStream inputStream, DataDictionaryHandler handler) throws IOException {
         parse(inputStream, Header.WITH_HEADER, handler);
     }
 
+    /**
+     * Parse the data dictionary from the specified input stream.
+     * @param inputStream The input stream
+     * @param header Header presence signifier
+     * @param handler A handler for handling parsed rows
+     */
     public void parse(InputStream inputStream, Header header, DataDictionaryHandler handler) throws IOException {
+        Objects.requireNonNull(inputStream);
+        Objects.requireNonNull(header);
+        Objects.requireNonNull(handler);
         CsvMapper mapper = new CsvMapper();
         mapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
         mapper.configure(CsvParser.Feature.IGNORE_TRAILING_UNMAPPABLE, true);
